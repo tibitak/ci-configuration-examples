a0eebd5ef3d7620ba0678718a207bc4b88c00423
tibor.takacs <tibor.takacs@SCI00193>
2026-02-23 13:39:21 +0100

    Add Code Checker to the build file.

--- buildfile.m
+++ buildfile.m
@@ -6,11 +6,15 @@ addpath("code");
 % Create a plan
 plan = buildplan(localfunctions);
 
+
+% Add the "check" task to identify code issues
+plan("check") = matlab.buildtool.tasks.CodeIssuesTask("check");
+
 % Add a task to run tests
 plan("test") = matlab.buildtool.tasks.TestTask("tests");
 
 % Make the "test" task the default task in the plan
-plan.DefaultTasks = "test";
+plan.DefaultTasks = "check";
 
 end
 
