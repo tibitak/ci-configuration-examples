e7b1dd6b62afff58a1bc65b13ada1d4dc3eb510a
tibor.takacs <tibor.takacs@SCI00193>
2026-02-23 13:46:12 +0100

    Default action = test

--- buildfile.m
+++ buildfile.m
@@ -6,15 +6,11 @@ addpath("code");
 % Create a plan
 plan = buildplan(localfunctions);
 
-
-% Add the "check" task to identify code issues
-plan("check") = matlab.buildtool.tasks.CodeIssuesTask("check");
-
 % Add a task to run tests
 plan("test") = matlab.buildtool.tasks.TestTask("tests");
 
 % Make the "test" task the default task in the plan
-plan.DefaultTasks = "check";
+plan.DefaultTasks = "test";
 
 end
 
